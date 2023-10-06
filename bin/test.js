dd52964f00cdb1fb3ec47faa2ac72ba6b4843784
Alex Fernández <alexfernandeznpm@gmail.com>
2023-10-06 18:56:57 +0200

    Added tests for pages

--- bin/test.js
+++ bin/test.js
@@ -1,6 +1,8 @@
-import test from '../test/api.js'
+import testApi from '../test/api.js'
+import testPages from '../test/pages.js'
 import {close} from '../lib/db/mongo.js'
 
-await test()
+await testApi()
+await testPages()
 await close()
 
