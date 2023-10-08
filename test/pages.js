70fafed0aee0fe4607166e62543df168f5f39490
Alex Fernández <alexfernandeznpm@gmail.com>
2023-10-08 16:54:22 +0200

    Created new endpoint /referer/show that does not require site

--- test/pages.js
+++ test/pages.js
@@ -20,8 +20,23 @@ async function testPage(url, check) {
 	console.assert(response.payload.includes(check), `did not page ${url}`)
 }
 
+async function testRedirect() {
+	const response = await app.inject({
+		url: `/referer/show`,
+		method: 'GET',
+		headers: {
+			'user-agent': userAgent,
+			referer: `https://${site}/whatever.page`,
+		},
+	})
+	console.assert(response.statusCode == 302, `could not redirect to referer`)
+	const location = response.headers.location || ''
+	console.assert(location.includes(site), `did not redirect to ${site}`)
+}
+
 export default async function test() {
 	await testHomePage()
 	await testStatsPage()
+	await testRedirect()
 }
 
