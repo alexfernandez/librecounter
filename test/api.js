f40b9ff6bcd449993483351378a4ab9fc2c81346
Alex Fernández <alexfernandeznpm@gmail.com>
2024-02-25 12:58:39 +0100

    Added test for cache

--- test/api.js
+++ test/api.js
@@ -21,42 +21,76 @@ async function testCountPage(page) {
 }
 
 async function testSiteStats() {
+	const stats = await testFetchSiteStats(`/${site}/siteStats`)
+	const day = getDay()
+	const found = stats.byDay.filter(dayStats => dayStats.day == day)
+	console.assert(found.length == 1, 'no data today')
+	console.assert(found[0].value > 0, 'no value today')
+	console.assert(stats.byPage[path], 'has no path')
+}
+
+async function testFetchSiteStats(url) {
 	const response = await app.inject({
-		url: `/${site}/siteStats`,
+		url,
 		method: 'GET',
 		headers: {'user-agent': 'testbot/1.0'},
 	})
 	console.assert(response.statusCode == 200, 'could not stats')
-	const result = response.json()
-	console.assert(result.byDay, 'has no days')
-	console.assert(Array.isArray(result.byDay), 'byDay is not an array')
-	const day = getDay()
-	const found = result.byDay.filter(dayStats => dayStats.day == day)
-	console.assert(found.length == 1, 'no data today')
-	console.assert(found[0].value > 0, 'no value today')
-	console.assert(result.byPage, 'has no pages')
-	console.assert(result.byPage[path], 'has no path')
+	const stats = response.json()
+	console.assert(stats.byDay, 'has no days')
+	console.assert(Array.isArray(stats.byDay), 'byDay is not an array')
+	console.assert(stats.byPage, 'has no pages')
+	return stats
 }
 
 async function testLastDays() {
-	const response = await app.inject({
-		url: `/${site}/siteStats?days=1`,
-		method: 'GET',
-		headers: {'user-agent': 'testbot/1.0'},
-	})
-	console.assert(response.statusCode == 200, 'could not stats')
-	const result = response.json()
-	console.assert(result.byDay, 'has no days')
-	console.assert(result.byDay.length == 1, 'should only have one day')
-	const dayStats = result.byDay[0]
+	const stats = await testFetchSiteStats(`/${site}/siteStats?days=1`)
+	console.assert(stats.byDay.length == 1, 'should only have one day')
+	const dayStats = stats.byDay[0]
 	const day = getDay()
 	console.assert(dayStats.day == day, 'should only have today')
 	console.assert(dayStats.value > 0, 'no value today')
+}
+
+async function testCache() {
+	const cachedPath = '/cached'
+	const day = getDay()
+	const firstStats = await testFetchSiteStats(`/${site}/siteStats`)
+	const firstFound = firstStats.byDay.filter(dayStats => dayStats.day == day)
+	console.assert(firstFound.length == 1, 'no data today')
+	console.assert(firstFound[0].value > 0, 'no value today')
+	const firstPage = firstStats.byPage[cachedPath]
+	console.assert(firstPage, 'has no path')
+	await testCountPage(cachedPath)
+	const secondStats = await testFetchSiteStats(`/${site}/siteStats`)
+	const secondFound = secondStats.byDay.filter(dayStats => dayStats.day == day)
+	console.assert(secondFound.length == 1, 'no data today')
+	console.assert(secondFound[0].value > 0, 'no value today')
+	console.assert(secondStats.byPage[cachedPath], 'has no path')
+	console.assert(secondFound[0].value == firstFound[0].value, 'cached day stats should not increase')
+	const secondPage = secondStats.byPage[cachedPath]
+	console.assert(secondPage == firstPage, 'cached page stats should not increase')
+	await sleep(1001)
+	const thirdStats = await testFetchSiteStats(`/${site}/siteStats`)
+	const thirdFound = thirdStats.byDay.filter(dayStats => dayStats.day == day)
+	console.assert(thirdFound.length == 1, 'no data today')
+	console.assert(thirdFound[0].value > 0, 'no value today')
+	console.assert(thirdFound[0].value == firstFound[0].value + 1, 'expired day stats should increase')
+	const thirdPage = thirdStats.byPage[cachedPath]
+	console.assert(thirdPage == firstPage + 1, 'expired page stats should increase')
+}
+
+/**
+ * Adapted from https://dev.to/codingwithadam/how-to-make-a-sleep-function-in-javascript-with-async-await-499b
+ */
+async function sleep(ms) {
+	return new Promise((resolve) =>setTimeout(resolve, ms))
 }
 
 export default async function test() {
 	await testCounter()
 	await testSiteStats()
 	await testLastDays()
+	await testCache()
 }
 
