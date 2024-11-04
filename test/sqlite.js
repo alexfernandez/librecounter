871839c6a8220181d0e0e97ac8481967c7143b55
Alex Fernández <alexfernandeznpm@gmail.com>
2024-11-05 00:46:10 +0100

    Create tables with index

--- test/sqlite.js
+++ test/sqlite.js
@@ -1,11 +1,12 @@
-import {initDb, incrementFields, findAll, close} from '../lib/db/sqlite.js'
+import {initDb, createIndex, incrementFields, findAll, close} from '../lib/db/sqlite.js'
 
 const configDb = 'test/test.db'
 
 
 async function testSimpleUpsert() {
+	await createIndex('test', {name: 1})
 	const query = {name: 'hi'}
-	await incrementFields('test', query, 'count')
+	await incrementFields('test', query, {count: 1})
 	const result = await findAll(query)
 	console.assert(result)
 	console.log(result)
