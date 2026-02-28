8f237521a0253d875b4b5162cbc8f6fdde73f101
Alex Fernández <alexfernandeznpm@gmail.com>
2026-02-28 14:14:18 +0100

    Feature: detect bots (#34)
    * Rename createCounter() to createDbTable()
    
    * Export createCounter() instead of class Counter
    
    * Store referer and user agent
    
    * Do not crash on invalid referer or user agent
    
    * Read bot and detect library as OS
    
    * Check for warnings in test with invalid domain
    
    * Store type as client type
    
    * Tests for devices

--- test/counter.js
+++ test/counter.js
@@ -36,9 +36,30 @@ async function testUniqueCounters() {
 	await testCounter(`/unique/outline-orange.svg`, 'unique outline orange')
 }
 
+async function testInvalidReferer() {
+	// dirty trick to catch warnings
+	const oldWarn = console.warn
+	const warnings = []
+	console.warn = warning => warnings.push(warning)
+	const response = await app.inject({
+		url: '/counter.svg',
+		method: 'GET',
+		headers: {
+			'user-agent': userAgent,
+			referer: `${site}`
+		},
+	})
+	console.warn = oldWarn
+	// end of dirty trick
+	console.assert(warnings.length, 'should have received warnings')
+	console.assert(response.statusCode == 200, `could not count invalid referer`)
+	console.assert(response.payload.includes('<svg'), `did not count invalid referer`)
+}
+
 export default async function test() {
 	await testSvgCounters()
 	await testOldStyle()
 	await testUniqueCounters()
+	await testInvalidReferer()
 }
 
