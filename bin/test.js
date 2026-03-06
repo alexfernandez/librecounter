28e7193070ad193eaa7e88e18e64dfa38892aaad
Alex Fernández <alexfernandeznpm@gmail.com>
2026-03-06 18:31:54 +0100

    Option to keep a site hidden from the homepage (#27)
    * Implement hidden counter
    
    * Rename hidden tests
    
    * Add hidden tests
    
    * Document on README page
    
    * v3.1.0
    
    * Fix counter
    
    * Refactor default dir
    
    * Default hidden svg with width=0
    
    * Validate custom files
    
    * Document a bit
    
    * Fix validation
    
    * Add image
    
    * Bump minor version

--- bin/test.js
+++ bin/test.js
@@ -1,7 +1,7 @@
 import testApi from '../test/api.js'
 import testPages from '../test/pages.js'
 import testCounter from '../test/counter.js'
-import testDomains from '../test/domain.js'
+import testHidden from '../test/hidden.js'
 import testSqlite from '../test/sqlite.js'
 import testDevices from '../test/device.js'
 import {close} from '../db/sqlite.js'
@@ -9,7 +9,7 @@ import {close} from '../db/sqlite.js'
 await testApi()
 await testPages()
 await testCounter()
-await testDomains()
+await testHidden()
 await testSqlite()
 await testDevices()
 close()
